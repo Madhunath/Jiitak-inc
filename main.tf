27de03e00beb03d210e83b48708b3453ce07bf42
Madhunath <146817676+Madhunath@users.noreply.github.com>
2024-02-13 00:03:16 +0530

    terraform file

--- main.tf
+++ main.tf
@@ -20,10 +20,25 @@ resource "aws_subnet" "proj_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "proj_igw" {
+  vpc_id = aws_vpc.proj_vpc.id
+}
+
+resource "aws_route_table" "proj_route_table" {
+  vpc_id = aws_vpc.proj_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.proj_igw.id
+  }
+}
+
 resource "aws_instance" "project_instance" {
   ami           = "ami-0ee4f2271a4df2d7d"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.proj_subnet.id
+  key_name      = "awskey"
+  associate_public_ip_address = true
 
   tags = {
     Name = "project-instance"
